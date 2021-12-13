227851878ca9168886a10ff3544997d1ef45ac18
fbn22 <frzadkowski@wp.pl>
2021-12-13 16:26:35 +0100

    gallery-update

--- js/main.js
+++ js/main.js
@@ -145,3 +145,55 @@ function appendMembers(members) {
   }
   document.querySelector("#members").innerHTML = htmlTemplate;
 }
+
+
+
+
+
+
+const URL =
+  "json/data.json";
+document.addEventListener("DOMContentLoaded", () => {
+  //set up the IntersectionObserver to load more images if the footer is visible.
+  //URL - https://gist.githubusercontent.com/prof3ssorSt3v3/1944e7ba7ffb62fe771c51764f7977a4/raw/c58a342ab149fbbb9bb19c94e278d64702833270/infinite.json
+  let options = {
+    root: null,
+    rootMargins: "0px",
+    threshold: 0.5
+  };
+  const observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(document.querySelector("footer"));
+  //an initial load of some data
+  getData();
+});
+
+function handleIntersect(entries) {
+  if (entries[0].isIntersecting) {
+    console.warn("something is intersecting with the viewport");
+    getData();
+  }
+}
+
+function getData() {
+  let content = document.getElementById("content");
+  console.log("fetch some JSON data");
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      // data.items[].img, data.items[].name
+      data.items.forEach(item => {
+        let fig = document.createElement("a");
+        let img = document.createElement("img");
+        img.src = item.img;
+        fig.appendChild(img);
+        content.appendChild(fig);
+      });
+    });
+}
+
+function setAttributeNode() {
+  let a = document.getElementsByTagName("a")[0];
+  let att = document.createAttribute("href");
+  att.value = "item.img";
+  a.setAttributeNode(att);
+}
