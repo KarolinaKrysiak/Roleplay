c695f320d4cb7cc67a900c84aeeda8e880ff3725
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2021-12-13 22:28:16 +0100

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 "use strict";
 
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNavX() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+/* Close when someone clicks on the lnk inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  window.scroll({
+    top: 0, 
+    left: 0, 
+   });
+}
+
 /*
 global variables
 */
